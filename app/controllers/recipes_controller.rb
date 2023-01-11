a589f94e074a2c02ffb1fbf3d5bfca9dbc9e0bb5
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2023-01-11 21:55:35 +0900

    新規作成　新規登録

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,8 +6,21 @@ class RecipesController < ApplicationController
   end
 
   def new
+    @recipe = Recipe.new
+  end
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user_id = current_user.id
+    @recipe.save
+    redirect_to recipe_path(@recipe), notice: "レシピを投稿しました"
   end
 
   def edit
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :body, :image)
   end
 end
