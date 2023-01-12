ef73b1a742a19bff4e3c12c8d13c62bf25d58ddd
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2023-01-12 15:25:21 +0900

    recipeのバリデーション設定　アクセス制限

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,20 +13,28 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
   def create
-    @recipe = Recipe.new(recipe_params)
-    @recipe.user_id = current_user.id
-    @recipe.save
-    redirect_to recipe_path(@recipe), notice: "レシピを投稿しました"
+    if @recipe = Recipe.new(recipe_params)
+      @recipe.save
+      redirect_to recipe_path(@recipe), notice: "レシピを投稿しました"
+    else
+      render :new
+    end
   end
 
   def edit
     @recipe = Recipe.find(params[:id])
+    if @recipe.user != current_user
+      redirect_to recipe_path, alert: "不正なアクセスです"
+    end
   end
 
   def update
     @recipe = Recipe.find(params[:id])
-    @recipe.update(recipe_params)
-    redirect_to recipe_path(@recipe), notice: "レシピを更新しました。"
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path(@recipe), notice: "レシピを更新しました。"
+    else
+      render :edit
+    end
   end
 
   def destroy
